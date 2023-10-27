bd1bffc60f277bc0bc5888703dfd5554534e93e1
RianVitor26 <rianvitorlhe@gmail.com>
2023-10-27 04:40:40 -0300

    feat: criando service do login

---
+++ my-app/src/services/authService.js
@@ -0,0 +1,6 @@
+import { api } from "./api"
+
+export const createSession = async (email, password) => {
+    const authenticatedUser = await api.post('/auth/login', {email, password})
+    return authenticatedUser.data
+}
